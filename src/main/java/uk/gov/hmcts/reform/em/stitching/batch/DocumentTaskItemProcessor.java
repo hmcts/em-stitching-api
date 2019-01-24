6220f2a103fafe366d8201592154847f0354f8b0
Linus Norton <linusnorton@gmail.com>
2019-01-24 14:47:31 +0000

    WIP

--- src/main/java/uk/gov/hmcts/reform/em/stitching/batch/DocumentTaskItemProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/batch/DocumentTaskItemProcessor.java
@@ -7,6 +7,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 import pl.touk.throwing.ThrowingConsumer;
 import pl.touk.throwing.ThrowingFunction;
+import rst.pdfbox.layout.elements.Paragraph;
+import rst.pdfbox.layout.text.BaseFont;
+import uk.gov.hmcts.reform.em.stitching.domain.BundleDocument;
 import uk.gov.hmcts.reform.em.stitching.domain.DocumentTask;
 import uk.gov.hmcts.reform.em.stitching.domain.enumeration.TaskState;
 import uk.gov.hmcts.reform.em.stitching.service.DmStoreDownloader;
@@ -14,6 +17,13 @@ import uk.gov.hmcts.reform.em.stitching.service.DmStoreUploader;
 import uk.gov.hmcts.reform.em.stitching.service.DocumentConversionService;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import rst.pdfbox.layout.elements.Document;
 
 public class DocumentTaskItemProcessor implements ItemProcessor<DocumentTask, DocumentTask> {
 
@@ -35,16 +45,19 @@ public class DocumentTaskItemProcessor implements ItemProcessor<DocumentTask, Do
 
     @Override
     public DocumentTask process(DocumentTask item) {
-        PDFMergerUtility merger = pdfMergerFactory.create();
+        final PDFMergerUtility merger = pdfMergerFactory.create();
 
         try {
+            final File tableOfContents = createTableOfContents(item.getBundle().getSortedItems());
+            merger.addSource(tableOfContents);
+
             dmStoreDownloader
                 .downloadFiles(item.getBundle().getSortedItems())
                 .map(ThrowingFunction.unchecked(documentConverter::convert))
                 .forEachOrdered(ThrowingConsumer.unchecked(merger::addSource));
 
             merger.mergeDocuments(MemoryUsageSetting.setupTempFileOnly());
-            File outputFile = new File(merger.getDestinationFileName());
+            final File outputFile = new File(merger.getDestinationFileName());
 
             dmStoreUploader.uploadFile(outputFile, item);
 
@@ -58,6 +71,26 @@ public class DocumentTaskItemProcessor implements ItemProcessor<DocumentTask, Do
         }
 
         return item;
+    }
+
+    private File createTableOfContents(Stream<BundleDocument> sortedItems) throws IOException {
+        Document document = new Document();
+        Paragraph paragraph = new Paragraph();
+        paragraph.addMarkup("Contents\n\n", 24, BaseFont.Helvetica);
+
+        for (BundleDocument doc : sortedItems.collect(Collectors.toList())) {
+            String content = String.format("{color:#ff5000}{link[#%s]}%s{link}{color:#000000}.%n", doc.getDocumentId(), doc.getDocTitle());
+            paragraph.addMarkup(content, 14, BaseFont.Helvetica);
+        }
+
+        document.add(paragraph);
+
+        final File tocFile = File.createTempFile("table-of-contents", ".pdf");
+        final OutputStream outputStream = new FileOutputStream(tocFile);
+
+        document.save(outputStream);
+
+        return tocFile;
     }
 
 }
