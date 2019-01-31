2fcb5c5dd6e78e1db4ea30f7a6820b2b51d34e7d
pawelkaczmarekuk <pawel.kaczmarek@gmail.com>
2019-01-31 11:25:20 +0100

    unit tests for the orchestrator

--- src/main/java/uk/gov/hmcts/reform/em/stitching/service/callback/impl/CcdCallbackHandlerServiceImpl.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/service/callback/impl/CcdCallbackHandlerServiceImpl.java
@@ -4,10 +4,9 @@ import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import uk.gov.hmcts.reform.em.stitching.service.callback.CasePropertyFinder;
-import uk.gov.hmcts.reform.em.stitching.service.callback.CcdCaseUpdater;
-import uk.gov.hmcts.reform.em.stitching.service.callback.CcdCallbackHandlerService;
 import uk.gov.hmcts.reform.em.stitching.service.callback.CcdCallbackDto;
-
+import uk.gov.hmcts.reform.em.stitching.service.callback.CcdCallbackHandlerService;
+import uk.gov.hmcts.reform.em.stitching.service.callback.CcdCaseUpdater;
 
 @Service
 @Transactional
