23195e0590189a504e3f88425dabddfb9187dac7
Linus Norton <linusnorton@gmail.com>
2019-02-07 17:32:25 +0000

    Add bundle title and description to table of contents (#27)
    * Add bundle title and description to table of contents
    
    * fix functional tests
    
    * keep sonar happy

--- src/main/java/uk/gov/hmcts/reform/em/stitching/pdf/PDFMerger.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/pdf/PDFMerger.java
@@ -3,30 +3,27 @@ package uk.gov.hmcts.reform.em.stitching.pdf;
 import org.apache.pdfbox.multipdf.PDFMergerUtility;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
-import org.apache.pdfbox.pdmodel.PDPageContentStream;
-import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.font.PDType1Font;
 import org.apache.pdfbox.pdmodel.interactive.action.PDActionGoTo;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDAnnotationLink;
 import org.apache.pdfbox.pdmodel.interactive.annotation.PDBorderStyleDictionary;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageXYZDestination;
 import org.springframework.stereotype.Service;
+import uk.gov.hmcts.reform.em.stitching.domain.Bundle;
 
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-import static uk.gov.hmcts.reform.em.stitching.pdf.PDFUtility.addCenterText;
-import static uk.gov.hmcts.reform.em.stitching.pdf.PDFUtility.getDocumentTitle;
+import static uk.gov.hmcts.reform.em.stitching.pdf.PDFUtility.*;
 
 @Service
 public class PDFMerger {
 
-    public File merge(List<PDDocument> documents) throws IOException {
+    public File merge(Bundle bundle, List<PDDocument> documents) throws IOException {
         StatefulPDFMerger statefulPDFMerger = new StatefulPDFMerger();
 
-        return statefulPDFMerger.merge(documents);
+        return statefulPDFMerger.merge(bundle, documents);
     }
 
     private class StatefulPDFMerger {
@@ -36,12 +33,9 @@ public class PDFMerger {
         private final PDPage page = new PDPage();
         private int currentPageNumber = 1;
 
-        public StatefulPDFMerger() {
-            document.addPage(page);
-        }
+        public File merge(Bundle bundle, List<PDDocument> documents) throws IOException {
+            setupFirstPage(bundle);
 
-        public File merge(List<PDDocument> documents) throws IOException {
-            addCenterText(document, page, "Table of contents");
             int documentIndex = 1;
 
             for (PDDocument d : documents) {
@@ -55,6 +49,13 @@ public class PDFMerger {
             document.close();
 
             return file;
+        }
+
+        private void setupFirstPage(Bundle bundle) throws IOException {
+            document.addPage(page);
+            addCenterText(document, page, bundle.getBundleTitle());
+            addText(document, page, bundle.getDescription(), 80);
+            addCenterText(document, page, "Contents", 100);
         }
 
         private void add(PDDocument newDoc, int documentIndex) throws IOException {
@@ -72,8 +73,8 @@ public class PDFMerger {
             final PDActionGoTo action = new PDActionGoTo();
             action.setDestination(destination);
 
-            final float yOffset = (float) documentIndex * LINE_HEIGHT;
-            final PDRectangle rectangle = new PDRectangle(45, 700 - yOffset, 500, LINE_HEIGHT);
+            final float yOffset = 150f + documentIndex * LINE_HEIGHT;
+            final PDRectangle rectangle = new PDRectangle(45, page.getMediaBox().getHeight() - yOffset, 500, LINE_HEIGHT);
 
             final PDBorderStyleDictionary underline = new PDBorderStyleDictionary();
             underline.setStyle(PDBorderStyleDictionary.STYLE_UNDERLINE);
@@ -86,13 +87,7 @@ public class PDFMerger {
 
             page.getAnnotations().add(link);
 
-            final PDPageContentStream stream = new PDPageContentStream(document, page, AppendMode.APPEND, true);
-            stream.beginText();
-            stream.setFont(PDType1Font.HELVETICA, 10);
-            stream.newLineAtOffset(50, 703 - yOffset);
-            stream.showText(documentTitle + ", p" + (currentPageNumber + 1));
-            stream.endText();
-            stream.close();
+            addText(document, page, documentTitle + ", p" + (currentPageNumber + 1), yOffset - 3);
         }
     }
 }
