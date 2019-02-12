b65242a56a74e6884665283c9a4379ec311ed23d
Jeroen Rijks <jeroen-rijks@hotmail.com>
2019-02-12 17:55:43 +0000

    add functional tests to check sortindex and duplicate docs. Remove unused testutils methods

--- src/aat/java/uk/gov/hmcts/reform/em/stitching/testutil/TestUtil.java
+++ src/aat/java/uk/gov/hmcts/reform/em/stitching/testutil/TestUtil.java
@@ -4,20 +4,16 @@ import com.warrenstrange.googleauth.GoogleAuthenticator;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
-import io.restassured.response.ResponseBody;
 import io.restassured.specification.RequestSpecification;
 import org.json.JSONObject;
 import org.springframework.http.MediaType;
-import org.springframework.web.client.HttpStatusCodeException;
 import uk.gov.hmcts.reform.em.stitching.service.dto.BundleDTO;
 import uk.gov.hmcts.reform.em.stitching.service.dto.BundleDocumentDTO;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
@@ -27,20 +23,29 @@ public class TestUtil {
     private String s2sToken;
     private String idamToken;
 
-    public File getDocumentBinary(String documentId) throws Exception {
-        Response response = s2sAuthRequest()
-            .header("user-roles", "caseworker")
-            .request("GET", Env.getDmApiUrl() + "/documents/" + documentId + "/binary");
 
-        Path tempPath = Paths.get(System.getProperty("java.io.tmpdir") + "/" + documentId + "-test.pdf");
+    public File downloadDocument(String documentURI) throws IOException {
+        byte[] byteArray = s2sAuthRequest()
+                .header("user-roles", "caseworker")
+                .header("Content-Type", MediaType.MULTIPART_FORM_DATA_VALUE)
+                .request("GET", documentURI + "/binary")
+                .getBody()
+                .asByteArray();
 
-        Files.copy(response.getBody().asInputStream(), tempPath, StandardCopyOption.REPLACE_EXISTING);
+        File tempFile = File.createTempFile("stitched-indexed-document", ".pdf");
 
-        return tempPath.toFile();
+        try (OutputStream outputStream = new FileOutputStream(tempFile)) {
+            outputStream.write(byteArray);
+        } catch (Exception e) {
+            System.out.println("Error message: " + e);
+        }
+        return tempFile;
     }
 
-    public String uploadDocument(String pdfName) {
-        String newDocUrl = s2sAuthRequest()
+
+
+        public String uploadDocument(String pdfName) {
+        return s2sAuthRequest()
             .header("Content-Type", MediaType.MULTIPART_FORM_DATA_VALUE)
             .multiPart("files", "test.pdf", ClassLoader.getSystemResourceAsStream(pdfName), "application/pdf")
             .multiPart("classification", "PUBLIC")
@@ -48,8 +53,6 @@ public class TestUtil {
             .getBody()
             .jsonPath()
             .get("_embedded.documents[0]._links.self.href");
-
-        return newDocUrl;
     }
 
     public String uploadDocument() {
@@ -117,7 +120,6 @@ public class TestUtil {
             .post(Env.getIdamURL() + "/testing-support/accounts");
 
     }
-
 
     public String getS2sToken() {
 
@@ -200,6 +202,41 @@ public class TestUtil {
             .get("_embedded.documents[0]._links.self.href");
 
         return newDocUrl;
+    }
+
+    public BundleDTO getTestBundleWithDuplicateBundleDocuments() {
+        BundleDTO bundle = new BundleDTO();
+        bundle.setBundleTitle("Bundle Title");
+        bundle.setDescription("This is the description of the bundle: it is great.");
+        List<BundleDocumentDTO> docs = new ArrayList<>();
+        docs.add(getTestBundleDocument(uploadDocument(), "Document 1"));
+        docs.add(getTestBundleDocument(uploadDocument(), "Document 1"));
+        bundle.setDocuments(docs);
+
+        return bundle;
+    }
+
+    public BundleDTO getTestBundleWithSortedDocuments() {
+        BundleDTO bundle = new BundleDTO();
+        bundle.setBundleTitle("Bundle Title");
+        bundle.setDescription("This is the description of the bundle: it is great.");
+        List<BundleDocumentDTO> docs = new ArrayList<>();
+        docs.add(getTestBundleDocumentWithSortIndices(uploadDocument("Document1.pdf"), "Document1.pdf", 2));
+        docs.add(getTestBundleDocumentWithSortIndices(uploadDocument("Document2.pdf"), "Document2.pdf", 1));
+        bundle.setDocuments(docs);
+
+        return bundle;
+    }
+
+    private BundleDocumentDTO getTestBundleDocumentWithSortIndices(String documentUrl, String title, int sortIndex) {
+        BundleDocumentDTO document = new BundleDocumentDTO();
+
+        document.setDocumentURI(documentUrl);
+        document.setDocTitle(String.format("Title (%s)", title));
+        document.setDocDescription(String.format("Description (%s)", title));
+        document.setSortIndex(sortIndex);
+
+        return document;
     }
 
     public Response pollUntil(String endpoint, Function<JsonPath, Boolean> evaluator) throws InterruptedException, IOException {
