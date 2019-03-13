ab0611aa8b120f1e93b2fe99782c3a2b3a0dc8e4
Jeroen Rijks <jeroen-rijks@hotmail.com>
2019-03-13 12:39:18 +0000

    revert appinsights, just update service-auth dep and code

--- src/main/java/uk/gov/hmcts/reform/em/stitching/config/ServiceTokenGeneratorConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/config/ServiceTokenGeneratorConfiguration.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.em.stitching.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
@@ -10,8 +9,6 @@ import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGenerator;
 import uk.gov.hmcts.reform.authorisation.generators.AuthTokenGeneratorFactory;
 
 @Configuration
-@Lazy
-@EnableFeignClients(basePackageClasses = ServiceAuthorisationApi.class)
 public class ServiceTokenGeneratorConfiguration {
 
     @Bean
