a959f1cc8b176003dcbfed1865853938e6c2608b
Jeroen Rijks <jeroen-rijks@hotmail.com>
2019-05-09 09:44:32 +0100

    rename final strings

--- src/test/java/uk/gov/hmcts/reform/em/stitching/service/impl/DmStoreUriFormatterTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/stitching/service/impl/DmStoreUriFormatterTest.java
@@ -5,31 +5,31 @@ import org.junit.Test;
 
 public class DmStoreUriFormatterTest {
 
-    private  String MOCK_BASE_URI = "http://test-dm-store-uri";
+    private final String mockBaseUri = "http://test-dm-store-uri";
 
-    private  String MOCK_DOC_DETAILS = "/documents/12345";
+    private final String mockDocumentDetails = "/documents/12345";
 
-    private  String BINARY = "/binary";
+    private final String binary = "/binary";
 
-    private  String MOCK_CORRUPTED_DOC_URI = "http://test-dm-store-uri:443/documents/12345";
+    private final String mockCorruptedDocumentUri = "http://test-dm-store-uri:443/documents/12345";
 
-    private DmStoreUriFormatter dmStoreUriFormatter = new DmStoreUriFormatter(MOCK_BASE_URI);
+    private DmStoreUriFormatter dmStoreUriFormatter = new DmStoreUriFormatter(mockBaseUri);
 
     @Test
     public void doesAppendBinary() {
-        String result = dmStoreUriFormatter.formatDmStoreUri(MOCK_CORRUPTED_DOC_URI);
-        Assert.assertEquals(MOCK_BASE_URI.concat(MOCK_DOC_DETAILS).concat(BINARY), result);
+        String result = dmStoreUriFormatter.formatDmStoreUri(mockCorruptedDocumentUri);
+        Assert.assertEquals(mockBaseUri.concat(mockDocumentDetails).concat(binary), result);
     }
 
     @Test
     public void doesNotAppendBinaryWhenAlreadyPresent() {
-        String result = dmStoreUriFormatter.formatDmStoreUri(MOCK_CORRUPTED_DOC_URI.concat(BINARY));
-        Assert.assertEquals(MOCK_BASE_URI.concat(MOCK_DOC_DETAILS).concat(BINARY), result);
+        String result = dmStoreUriFormatter.formatDmStoreUri(mockCorruptedDocumentUri.concat(binary));
+        Assert.assertEquals(mockBaseUri.concat(mockDocumentDetails).concat(binary), result);
     }
 
     @Test
     public void doesNotAppendBinaryWhenNoDocumentsInString() {
-        String mockUri = MOCK_BASE_URI.concat("/12345");
+        String mockUri = mockBaseUri.concat("/12345");
         Assert.assertEquals(mockUri, dmStoreUriFormatter.formatDmStoreUri(mockUri));
     }
 }
