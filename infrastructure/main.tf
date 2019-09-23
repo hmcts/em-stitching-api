6180e606b99e0169fbaa1b1f09261e990e265803
Linus Norton <linusnorton@gmail.com>
2019-09-23 10:49:06 +0100

    Save DB creds in local vault

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -185,23 +185,23 @@ resource "azurerm_key_vault_secret" "POSTGRES-USER" {
 resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
   name = "${var.component}-POSTGRES-PASS"
   value = "${module.db.postgresql_password}"
-  key_vault_id = "${data.azurerm_key_vault.shared_key_vault.id}"
+  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
   name = "${var.component}-POSTGRES-HOST"
   value = "${module.db.host_name}"
-  key_vault_id = "${data.azurerm_key_vault.shared_key_vault.id}"
+  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
   name = "${var.component}-POSTGRES-PORT"
   value = "${module.db.postgresql_listen_port}"
-  key_vault_id = "${data.azurerm_key_vault.shared_key_vault.id}"
+  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
 }
 
 resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
   name = "${var.component}-POSTGRES-DATABASE"
   value = "${module.db.postgresql_database}"
-  key_vault_id = "${data.azurerm_key_vault.shared_key_vault.id}"
+  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
 }
