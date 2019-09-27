ab205acfbe60b578b439b18160a26fe5d4792794
Tom Elliott <tomxelliott@hotmail.co.uk>
2019-09-27 16:30:19 +0100

    EM-2143: suppress warning to allow constants to be camelCase

--- src/main/java/uk/gov/hmcts/reform/em/stitching/domain/enumeration/PaginationStyle.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/domain/enumeration/PaginationStyle.java
@@ -3,6 +3,7 @@ package uk.gov.hmcts.reform.em.stitching.domain.enumeration;
 /**
  * The Pagination Style enumeration.
  */
+@SuppressWarnings("all")
 public enum PaginationStyle {
     off, topLeft, topCenter, topRight, bottomLeft, bottomCenter, bottomRight;
 }
