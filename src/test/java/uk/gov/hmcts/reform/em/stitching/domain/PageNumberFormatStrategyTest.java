76c4d63821b8eb12968ce03c20bd4de58a2a7358
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2019-10-03 15:32:10 +0100

    Added in Test

---
+++ src/test/java/uk/gov/hmcts/reform/em/stitching/domain/PageNumberFormatStrategyTest.java
@@ -0,0 +1,21 @@
+package uk.gov.hmcts.reform.em.stitching.domain;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class PageNumberFormatStrategyTest {
+
+    private static final int noOfPages = 10;
+    private static final int pageNumber = 13;
+    private static final String pageRange = (pageNumber + 1) + " - " + (pageNumber + noOfPages);
+
+    @Test
+    public void testPageRange(){
+        assertEquals(pageRange, PageNumberFormatStrategy.pageRange().getPageNumber(pageNumber, noOfPages));
+    }
+
+    @Test
+    public void testNumberOfPages(){
+        assertEquals(String.valueOf(noOfPages), PageNumberFormatStrategy.numberOfPages().getPageNumber(pageNumber, noOfPages));
+    }
+}
