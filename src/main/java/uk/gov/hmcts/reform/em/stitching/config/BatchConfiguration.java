cbe8436a3f86959f82b172a73904d4e926a6b5d9
Linus Norton <linusnorton@gmail.com>
2019-10-04 10:11:49 +0100

    move to tmp folder

--- src/main/java/uk/gov/hmcts/reform/em/stitching/config/BatchConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/config/BatchConfiguration.java
@@ -76,7 +76,7 @@ public class BatchConfiguration {
 
     @Bean
     public JpaItemWriter itemWriter() {
-        System.setProperty("pdfbox.fontcache","/opt/app/");
+        System.setProperty("pdfbox.fontcache","/tmp/");
         JpaItemWriter writer = new JpaItemWriter<DocumentTask>();
         writer.setEntityManagerFactory(entityManagerFactory);
         return writer;
