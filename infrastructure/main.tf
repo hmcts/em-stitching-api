8fef2378c32a62df32ab64cebc50a469e5b0c97a
Damon Green <damon.green@btinternet.com>
2020-02-19 13:33:11 +0000

    remove app section, add create rg and comment vault copy

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -83,57 +83,57 @@ data "azurerm_key_vault" "local_key_vault" {
   depends_on = [module.local_key_vault]
 }
 
-//resource "azurerm_key_vault_secret" "local_s2s_key" {
-//  name         = "microservicekey-em-stitching-api"
-//  value        = "${data.azurerm_key_vault_secret.s2s_key.value}"
-//  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
-//}
-//
-//# Copy docmosis keys to local
-//resource "azurerm_key_vault_secret" "local_docmosis_access_key" {
-//  name         = "docmosis-access-key"
-//  value        = "${data.azurerm_key_vault_secret.docmosis_access_key.value}"
-//  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
-//}
-//
-//# Load AppInsights key from rpa vault
-//data "azurerm_key_vault_secret" "app_insights_key" {
-//  name         = "AppInsightsInstrumentationKey"
-//  key_vault_id = "${data.azurerm_key_vault.product.id}"
-//}
-//
-//resource "azurerm_key_vault_secret" "local_app_insights_key" {
-//  name         = "AppInsightsInstrumentationKey"
-//  value        = "${data.azurerm_key_vault_secret.app_insights_key.value}"
-//  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
-//}
-//
-//resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-//  name = "${var.component}-POSTGRES-USER"
-//  value = "${module.db.user_name}"
-//  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
-//}
-//
-//resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-//  name = "${var.component}-POSTGRES-PASS"
-//  value = "${module.db.postgresql_password}"
-//  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
-//}
-//
-//resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-//  name = "${var.component}-POSTGRES-HOST"
-//  value = "${module.db.host_name}"
-//  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
-//}
-//
-//resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
-//  name = "${var.component}-POSTGRES-PORT"
-//  value = "${module.db.postgresql_listen_port}"
-//  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
-//}
-//
-//resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-//  name = "${var.component}-POSTGRES-DATABASE"
-//  value = "${module.db.postgresql_database}"
-//  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
-//}
+#resource "azurerm_key_vault_secret" "local_s2s_key" {
+#  name         = "microservicekey-em-stitching-api"
+#  value        = "${data.azurerm_key_vault_secret.s2s_key.value}"
+#  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+#}
+
+# Copy docmosis keys to local
+#resource "azurerm_key_vault_secret" "local_docmosis_access_key" {
+#  name         = "docmosis-access-key"
+#  value        = "${data.azurerm_key_vault_secret.docmosis_access_key.value}"
+#  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+#}
+
+# Load AppInsights key from rpa vault
+#data "azurerm_key_vault_secret" "app_insights_key" {
+#  name         = "AppInsightsInstrumentationKey"
+#  key_vault_id = "${data.azurerm_key_vault.product.id}"
+#}
+
+#resource "azurerm_key_vault_secret" "local_app_insights_key" {
+#  name         = "AppInsightsInstrumentationKey"
+#  value        = "${data.azurerm_key_vault_secret.app_insights_key.value}"
+#  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+#}
+
+#resource "azurerm_key_vault_secret" "POSTGRES-USER" {
+#  name = "${var.component}-POSTGRES-USER"
+#  value = "${module.db.user_name}"
+#  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+#}
+
+#resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
+#  name = "${var.component}-POSTGRES-PASS"
+#  value = "${module.db.postgresql_password}"
+#  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+#}
+
+#resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
+#  name = "${var.component}-POSTGRES-HOST"
+#  value = "${module.db.host_name}"
+#  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+#}
+
+#resource "azurerm_key_vault_secret" "POSTGRES_PORT" {
+#  name = "${var.component}-POSTGRES-PORT"
+#  value = "${module.db.postgresql_listen_port}"
+#  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+#}
+
+#resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
+#  name = "${var.component}-POSTGRES-DATABASE"
+#  value = "${module.db.postgresql_database}"
+#  key_vault_id = "${data.azurerm_key_vault.local_key_vault.id}"
+#}
