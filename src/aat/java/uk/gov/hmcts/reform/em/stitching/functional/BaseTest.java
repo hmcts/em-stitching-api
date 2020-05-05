84ad59e7c31b21e1b32f38590369da3297436aed
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2020-05-05 21:32:35 +0100

    EM-2720:Serenity Reports for Stiching

--- src/aat/java/uk/gov/hmcts/reform/em/stitching/functional/BaseTest.java
+++ src/aat/java/uk/gov/hmcts/reform/em/stitching/functional/BaseTest.java
@@ -4,13 +4,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.em.EmTestConfig;
 import uk.gov.hmcts.reform.em.stitching.testutil.TestUtil;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+
 
 @SpringBootTest(classes = {TestUtil.class, EmTestConfig.class})
 @PropertySource(value = "classpath:application.yml")
-@RunWith(SpringRunner.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 public abstract class BaseTest {
 
     @Autowired
