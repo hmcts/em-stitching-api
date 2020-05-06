01d312e513e76499801284a2d15efb431caac248
isha2040 <58481974+isha2040@users.noreply.github.com>
2020-05-06 16:00:35 +0100

    EM-2885 Added code to integrate pacts in pipeline and publish to broker

--- src/contractTest/java/uk/gov/hmcts/reform/em/stitching/IdamConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/stitching/IdamConsumerTest.java
@@ -36,7 +36,7 @@ public class IdamConsumerTest {
     private static final String CLIENT_REDIRECT_URI = "/oauth2redirect";
     private static final String ACCESS_TOKEN = "111";
 
-    @Pact(provider = "idam_api", consumer = "em_stitching_api")
+    @Pact(provider = "Idam_api", consumer = "em_stitching_api")
     public RequestResponsePact executeGetUserDetailsAndGet200(PactDslWithProvider builder) {
 
         Map<String, String> headers = Maps.newHashMap();
@@ -91,7 +91,7 @@ public class IdamConsumerTest {
 
     }
 
-    @Pact(provider = "idam_api", consumer = "em_stitching_api")
+    @Pact(provider = "Idam_api", consumer = "em_stitching_api")
     public RequestResponsePact executeGetIdamAccessTokenAndGet200(PactDslWithProvider builder) throws JSONException {
 
         Map<String, String> headers = Maps.newHashMap();
