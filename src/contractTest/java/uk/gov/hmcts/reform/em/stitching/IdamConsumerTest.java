3eb392ef8a1dbc6762b590477d1825713e382ec2
Kasi Subramaniam <Kasi.Subramaniam@HMCTS.NET>
2020-08-06 14:23:10 +0100

    EM-3079: Updates to tests and build.gradle files

--- src/contractTest/java/uk/gov/hmcts/reform/em/stitching/IdamConsumerTest.java
+++ src/contractTest/java/uk/gov/hmcts/reform/em/stitching/IdamConsumerTest.java
@@ -6,8 +6,6 @@ import java.util.Map;
 import java.util.TreeMap;
 
 import au.com.dius.pact.consumer.MockServer;
-import au.com.dius.pact.consumer.dsl.DslPart;
-import au.com.dius.pact.consumer.dsl.PactDslJsonArray;
 import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
 import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
 import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
@@ -15,36 +13,21 @@ import au.com.dius.pact.consumer.junit5.PactTestFor;
 import au.com.dius.pact.core.model.RequestResponsePact;
 import au.com.dius.pact.core.model.annotations.Pact;
 import com.google.common.collect.Maps;
+import io.restassured.http.ContentType;
 import net.serenitybdd.rest.SerenityRest;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(PactConsumerTestExt.class)
 @ExtendWith(SpringExtension.class)
 public class IdamConsumerTest {
 
-    private static final String IDAM_DETAILS_URL = "/details";
     private static final String IDAM_OPENID_TOKEN_URL = "/o/token";
-    private static final String CLIENT_REDIRECT_URI = "/oauth2redirect";
-    private static String ACCESS_TOKEN = "111";
-
-    @Value("pact")
-    String client_id;
-
-    @Value("pactsecret")
-    String client_secret;
-
-    @Value("http://www.dummy-pact-service.com/callback")
-    String redirect_uri;
 
     @Pact(provider = "Idam_api", consumer = "em_stitching_api")
     public RequestResponsePact executeGetIdamAccessTokenAndGet200(PactDslWithProvider builder) {
@@ -66,10 +49,12 @@ public class IdamConsumerTest {
 
         return builder
                 .given("a user exists", params)
-                .uponReceiving("Provider takes user/pwd and returns Access Token to Annotation API")
+                .uponReceiving("Provider takes user/pwd and returns Access Token to DocAssembly API")
                 .path(IDAM_OPENID_TOKEN_URL)
                 .method(HttpMethod.POST.toString())
-                .body("redirect_uri=http%3A%2F%2Fwww.dummy-pact-service.com%2Fcallback&client_id=pact&grant_type=password&username=emCaseOfficer%40email.net&password=Password123&client_secret=pactsecret&scope=openid profile roles","application/x-www-form-urlencoded")
+                .body("redirect_uri=http%3A%2F%2Fwww.dummy-pact-service.com%2Fcallback"
+                        + "&client_id=pact&grant_type=password&username=emCaseOfficer%40email.net&password=Password123"
+                        + "&client_secret=pactsecret&scope=openid profile roles","application/x-www-form-urlencoded")
                 .willRespondWith()
                 .status(HttpStatus.OK.value())
                 .headers(responseheaders)
@@ -84,7 +69,7 @@ public class IdamConsumerTest {
         String actualResponseBody =
                 SerenityRest
                         .given()
-                        .contentType(io.restassured.http.ContentType.URLENC)
+                        .contentType(ContentType.URLENC)
                         .formParam("redirect_uri", "http://www.dummy-pact-service.com/callback")
                         .formParam("client_id", "pact")
                         .formParam("grant_type", "password")
@@ -98,76 +83,8 @@ public class IdamConsumerTest {
 
         JSONObject response = new JSONObject(actualResponseBody);
 
-        ACCESS_TOKEN = response.getString("access_token");
         assertThat(response).isNotNull();
         assertThat(response.getString("access_token")).isNotBlank();
-
-    }
-
-    @Pact(provider = "Idam_api", consumer = "em_stitching_api")
-    public RequestResponsePact executeGetUserDetailsAndGet200(PactDslWithProvider builder) throws JSONException {
-
-        Map<String, String> requestHeaders = Maps.newHashMap();
-        requestHeaders.put(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-        Map<String, Object> params = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-
-        params.put("redirect_uri", "http://www.dummy-pact-service.com/callback");
-        params.put("client_id", "pact");
-        params.put("client_secret", "pactsecret");
-        params.put("scope", "openid profile roles");
-        params.put("username", "emCaseOfficer@email.net");
-        params.put("password", "Password123");
-
-        Map<String, String> responseheaders = Maps.newHashMap();
-        responseheaders.put("Content-Type", "application/json");
-
-        return builder
-                .given("I have obtained an access_token as a user",params)
-                .uponReceiving("Provider returns user info to Annotation API")
-                .path(IDAM_DETAILS_URL)
-                .headers("Authorization","Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdjEre")
-                .method(HttpMethod.GET.toString())
-                .willRespondWith()
-                .status(HttpStatus.OK.value())
-                .headers(responseheaders)
-                .body(createUserInfoResponse())
-                .toPact();
-    }
-
-    @Test
-    @PactTestFor(pactMethod = "executeGetUserDetailsAndGet200")
-    public void should_get_user_details_with_access_token(MockServer mockServer)
-        throws JSONException {
-        Map<String, String> headers = Maps.newHashMap();
-        headers.put(HttpHeaders.AUTHORIZATION, "Bearer eyJ0eXAiOiJKV1QiLCJraWQiOiJiL082T3ZWdjEre");
-        headers.put(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-
-        String detailsResponseBody =
-                SerenityRest
-                        .given()
-                        .headers(headers)
-                        .when()
-                        .get(mockServer.getUrl() + IDAM_DETAILS_URL)
-                        .then()
-                        .statusCode(200)
-                        .and()
-                        .extract()
-                        .body()
-                        .asString();
-
-        JSONObject response = new JSONObject(detailsResponseBody);
-
-        assertThat(detailsResponseBody).isNotNull();
-        assertThat(response).hasNoNullFieldsOrProperties();
-        assertThat(response.getString("uid")).isNotBlank();
-        assertThat(response.getString("given_name")).isNotBlank();
-        assertThat(response.getString("family_name")).isNotBlank();
-        JSONArray rolesArr = response.getJSONArray("roles");
-        assertThat(rolesArr).isNotNull();
-        assertThat(rolesArr.length()).isNotZero();
-        assertThat(rolesArr.get(0).toString()).isNotBlank();
-
-
     }
 
     private PactDslJsonBody createAuthResponse() {
@@ -179,31 +96,6 @@ public class IdamConsumerTest {
             .stringType("id_token", "some-value")
             .stringType("token_type", "Bearer")
             .stringType("expires_in","12345");
-
-    }
-
-    private PactDslJsonBody createUserDetailsResponse() {
-        PactDslJsonArray array = new PactDslJsonArray().stringValue("caseofficer-em");
-
-        return new PactDslJsonBody()
-            .stringType("id", "123")
-            .stringType("email", "em-caseofficer@fake.hmcts.net")
-            .stringType("forename", "Case")
-            .stringType("surname", "Officer")
-            .stringType("roles", array.toString());
-
-    }
-
-
-    private DslPart createUserInfoResponse() {
-
-        return new PactDslJsonBody()
-                .stringType("uid", "1234-2345-3456-4567")
-                .stringType("given_name", "emCaseOfficer")
-                .stringType("family_name", "Jar")
-                .array("roles")
-                .stringType("citizen")
-                .closeArray();
 
     }
 
