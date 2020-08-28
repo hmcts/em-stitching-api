ef12928eb7aa864bd2e151d1961666c8ec8e8a28
benouaer <tech@benouaer.com>
2020-08-28 18:50:53 +0100

    fix failing tests

--- src/test/java/uk/gov/hmcts/reform/em/stitching/pdf/PDFOutlineTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/stitching/pdf/PDFOutlineTest.java
@@ -2,23 +2,20 @@ package uk.gov.hmcts.reform.em.stitching.pdf;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageDestination;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import uk.gov.hmcts.reform.em.stitching.Application;
-
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.EmptyStackException;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = Application.class)
 public class PDFOutlineTest {
     private static final File FILE_1 = new File(
             ClassLoader.getSystemResource("TEST_INPUT_FILE.pdf").getPath()
@@ -31,12 +28,13 @@ public class PDFOutlineTest {
         document.addPage(new PDPage());
 
         pdfOutline.addBundleItem("Bundle");
+        pdfOutline.setRootOutlineItemDest();
 
         PDDocumentOutline documentOutline = document.getDocumentCatalog().getDocumentOutline();
 
         assertEquals(documentOutline.getFirstChild().getTitle(), "Bundle");
-        assertNotEquals(documentOutline.getFirstChild().getDestination(), null);
-        assertNotEquals(documentOutline.getFirstChild(), null);
+        assertNotNull(documentOutline.getFirstChild().getDestination());
+        assertNotNull(documentOutline.getFirstChild());
     }
 
     @Test
@@ -48,51 +46,40 @@ public class PDFOutlineTest {
 
         pdfOutline.addBundleItem("Bundle");
         pdfOutline.addItem(0, "Folder Item 1");
-        pdfOutline.addItem(0, "Sub Item 1");
         pdfOutline.addItem(1, "Folder Item 2");
-        pdfOutline.addItem(1, "Sub Item 2");
+        pdfOutline.setRootOutlineItemDest();
 
         PDDocumentOutline documentOutline = document.getDocumentCatalog().getDocumentOutline();
         PDOutlineItem bundleOutline = documentOutline.getFirstChild();
 
-        assertNotEquals(bundleOutline, null);
+        assertNotNull(bundleOutline);
+        assertNotNull(bundleOutline.getDestination());
         assertEquals(bundleOutline.getTitle(), "Bundle");
-        assertNotEquals(bundleOutline.getDestination(), null);
-        assertEquals(bundleOutline.getFirstChild().getTitle(), "Folder Item 1");
-        assertEquals(bundleOutline.getFirstChild().getFirstChild().getTitle(), "Sub Item 1");
-        assertEquals(bundleOutline.getFirstChild().getNextSibling().getTitle(), "Folder Item 2");
-        assertEquals(bundleOutline.getFirstChild().getNextSibling().getFirstChild().getTitle(), "Sub Item 2");
+        assertEquals(bundleOutline.getNextSibling().getTitle(), "Folder Item 1");
+        assertEquals(bundleOutline.getNextSibling().getNextSibling().getTitle(), "Folder Item 2");
     }
 
     @Test
-    public void mergeOutlineFromAnotherDocument() throws IOException {
+    public void createHeadingItem() throws IOException {
         PDDocument document = new PDDocument();
         PDFOutline pdfOutline = new PDFOutline(document);
         document.addPage(new PDPage());
+        document.addPage(new PDPage());
 
         pdfOutline.addBundleItem("Bundle");
-        pdfOutline.addItem(0, "Folder Item 1");
-
-        PDDocument newDoc = new PDDocument();
-        newDoc.addPage(new PDPage());
-        PDDocumentOutline newDocOutline = new PDDocumentOutline();
-        PDOutlineItem outlineItem = new PDOutlineItem();
-        outlineItem.setTitle("New Doc Outline");
-        outlineItem.setDestination(newDoc.getPage(0));
-        newDocOutline.addFirst(outlineItem);
-        newDoc.getDocumentCatalog().setDocumentOutline(newDocOutline);
+        PDOutlineItem item = pdfOutline.createHeadingItem(document.getPage(0), "heading item");
+        document.getDocumentCatalog().getDocumentOutline().addFirst(item);
 
         PDDocumentOutline documentOutline = document.getDocumentCatalog().getDocumentOutline();
         PDOutlineItem bundleOutline = documentOutline.getFirstChild();
 
-        assertNotEquals(bundleOutline, null);
-        assertEquals(bundleOutline.getTitle(), "Bundle");
-        assertNotEquals(bundleOutline.getDestination(), null);
-        assertEquals(bundleOutline.getFirstChild().getTitle(), "Folder Item 1");
-        assertEquals(bundleOutline.getFirstChild().getFirstChild().getTitle(), "New Doc Outline");
+        assertNotNull(bundleOutline);
+        assertEquals("Bundle", bundleOutline.getNextSibling().getTitle());
+        assertEquals("heading item", bundleOutline.getTitle());
+        assertEquals(0, ((PDPageDestination) bundleOutline.getDestination()).retrievePageNumber());
     }
 
-    @Test(expected = EmptyStackException.class)
+    @Test(expected = IndexOutOfBoundsException.class)
     public void addItemWithInvalidPage() {
         PDDocument document = new PDDocument();
         PDFOutline pdfOutline = new PDFOutline(document);
