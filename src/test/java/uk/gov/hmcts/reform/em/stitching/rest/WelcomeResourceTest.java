c3896216488ebeea4a44d652beab8a85329ddcf1
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2020-09-21 15:03:09 +0100

    Resolved checkstyle issue

--- src/test/java/uk/gov/hmcts/reform/em/stitching/rest/WelcomeResourceTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/stitching/rest/WelcomeResourceTest.java
@@ -3,7 +3,6 @@ package uk.gov.hmcts.reform.em.stitching.rest;
 import org.junit.Test;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertEquals;
