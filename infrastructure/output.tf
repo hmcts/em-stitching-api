84e3e24e9a81b6631695acaca99939ae29439e4e
rishisharma <rishi.sharma@hmcts.net>
2021-10-19 10:34:45 +0100

    EM-3267: remove deprecated ase related tf code

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -5,11 +5,3 @@ output "idam_api_base_uri" {
 output "open_id_api_base_uri" {
   value = "${var.open_id_api_base_uri}"
 }
-
-output "enable_idam_health_check" {
-  value = "${var.enable_idam_healthcheck}"
-}
-
-output "enable_idam_healthcheck" {
-  value = "${var.enable_idam_healthcheck}"
-}
