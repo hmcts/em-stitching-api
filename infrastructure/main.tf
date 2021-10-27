7fdfa23ef18df59858842d1bcdf1001df8db4f32
rishisharma <rishi.sharma@hmcts.net>
2021-10-27 10:35:09 +0100

    EM-3267: user cnp postgres module master

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -34,12 +34,6 @@ module "db" {
   subscription = var.subscription
 }
 
-data "azurerm_subnet" "postgres" {
-  name                 = "core-infra-subnet-0-${var.env}"
-  resource_group_name  = "core-infra-${var.env}"
-  virtual_network_name = "core-infra-vnet-${var.env}"
-}
-
 module "db-v11" {
   source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
   product            = var.product
@@ -50,7 +44,6 @@ module "db-v11" {
   postgresql_user    = var.postgresql_user_v11
   database_name      = var.database_name_v11
   postgresql_version = "11"
-  subnet_id          = data.azurerm_subnet.postgres.id
   sku_name           = "GP_Gen5_2"
   sku_tier           = "GeneralPurpose"
   common_tags        = var.common_tags
