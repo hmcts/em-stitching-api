bc99e98e6c269c841ebb51c90d817fa6f2d964ed
mdayican <mustafa.dayican@hmcts.net>
2022-09-13 11:09:39 +0100

    EM-4595 merge

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -120,17 +120,6 @@ resource "azurerm_key_vault_secret" "local_app_insights_key" {
   value        = data.azurerm_key_vault_secret.app_insights_key.value
   key_vault_id = data.azurerm_key_vault.local_key_vault.id
 }
-    
-data "azurerm_key_vault_secret" "app_insights_connection_string" {
-  name         = "app-insights-connection-string"
-  key_vault_id = data.azurerm_key_vault.product.id
-}
-
-resource "azurerm_key_vault_secret" "local_app_insights_connection_string" {
-  name         = "app-insights-connection-string"
-  value        = data.azurerm_key_vault_secret.app_insights_connection_string.value
-  key_vault_id = data.azurerm_key_vault.local_key_vault.id
-}    
 
 data "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
