348ba288e804112c1ffc3d0e015b628a31f6488a
mdayican <mustafa.dayican@hmcts.net>
2022-09-29 10:20:52 +0100

    EM-4586 change http client

--- src/main/java/uk/gov/hmcts/reform/em/stitching/config/HttpClientConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/config/HttpClientConfiguration.java
@@ -1,6 +1,12 @@
 package uk.gov.hmcts.reform.em.stitching.config;
 
+import feign.Client;
+import feign.httpclient.ApacheHttpClient;
 import okhttp3.OkHttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,6 +21,28 @@ public class HttpClientConfiguration {
             .connectTimeout(10, TimeUnit.MINUTES)
             .readTimeout(10, TimeUnit.MINUTES)
             .retryOnConnectionFailure(true)
+            .build();
+    }
+
+    @Bean
+    public Client getFeignHttpClient(
+        @Value("${feign.httpClient.connectTimeout}") final int connectTimeout,
+        @Value("${feign.httpClient.socketTimeout}") final int socketTimeout
+    ) {
+        return new ApacheHttpClient(getHttpClient(connectTimeout, socketTimeout));
+    }
+
+    private CloseableHttpClient getHttpClient(int connectTimeout, int socketTimeout) {
+        RequestConfig config = RequestConfig.custom()
+            .setConnectTimeout(connectTimeout)
+            .setConnectionRequestTimeout(connectTimeout)
+            .setSocketTimeout(socketTimeout)
+            .build();
+
+        return HttpClientBuilder
+            .create()
+            .useSystemProperties()
+            .setDefaultRequestConfig(config)
             .build();
     }
 
