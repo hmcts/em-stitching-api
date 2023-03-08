30903f701bd186d1e9a8e324c486268290638ebf
yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>
2023-03-08 16:11:46 +0000

    Updated the logic for stitched file name (#1422)
    * Updated the logic for stitched file name
    
    * Resolved CVE
    
    * Updated CVE list

--- src/main/java/uk/gov/hmcts/reform/em/stitching/service/StringFormattingUtils.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/service/StringFormattingUtils.java
@@ -1,8 +1,18 @@
 package uk.gov.hmcts.reform.em.stitching.service;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class StringFormattingUtils {
 
-    public static String ensureStringEndsWithSuffix(String string, String suffix) {
-        return string.endsWith(suffix) ? string : string + suffix;
+    public static final String SUFFIX = ".pdf";
+    public static final String PREFIX = "stitched";
+
+    public static String generateFileName(String fileName) {
+        if (StringUtils.isNotBlank(fileName)) {
+            return StringUtils.endsWithIgnoreCase(fileName, SUFFIX)
+                    ? fileName : fileName + SUFFIX;
+        } else {
+            return PREFIX + System.currentTimeMillis() + SUFFIX;
+        }
     }
 }
