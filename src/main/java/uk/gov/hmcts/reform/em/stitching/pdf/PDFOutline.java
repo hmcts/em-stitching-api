ee6693d8a95c211fba7ad85114a7a3e64ff14d08
MUSTAFA <mustafa.dayican1@hmcts.net>
2023-03-16 09:46:24 +0000

    EM-5145 fix getOutlinePage , ignore Nameddestinations (#1439)
    * fix getOutlinePage , ignore Nameddestinations
    
    * workaround for checkstyle
    
    * workaround for checkstyle-2
    
    * remove log
    
    * fix checkstyle
    
    ---------
    
    Co-authored-by: mdayican <mustafa.dayican@hmcts.net>
    Co-authored-by: yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/em/stitching/pdf/PDFOutline.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/pdf/PDFOutline.java
@@ -5,6 +5,7 @@ import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.cos.COSNull;
 import org.apache.pdfbox.multipdf.PDFCloneUtility;
 import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDDestination;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageDestination;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem;
@@ -145,8 +146,7 @@ public class PDFOutline {
 
         //document coversheet outline or doc outline should already be there.
         destLastOutlineItem = findPdOutlineItem(destLastOutlineItem, key);
-        for (PDOutlineItem item : srcOutline.children())
-        {
+        for (PDOutlineItem item : srcOutline.children()) {
             // get each child, clone its dictionary, remove siblings info,
             // append outline item created from there
             COSDictionary clonedDict = (COSDictionary) cloner.cloneForNewDocument(item);
@@ -177,15 +177,17 @@ public class PDFOutline {
     }
 
     public int getOutlinePage(PDOutlineItem outlineItem) {
-        PDPageDestination dest = null;
         try {
-            dest = (PDPageDestination) outlineItem.getDestination();
-            log.info("outlineItem Title: {}, destination is null : {}" + outlineItem.getTitle(), (dest == null));
-            return dest == null ? -1 : Math.max(dest.retrievePageNumber(), 0);
-        } catch (IOException e) {
-            log.error("Error message: " + e);
-            return -1;
+            PDDestination pdDestination = outlineItem.getDestination();
+            if (pdDestination instanceof PDPageDestination) {
+                var dest = (PDPageDestination) pdDestination;
+                log.info("outlineItem Title: {}, destination is null : {}" + outlineItem.getTitle(), (dest == null));
+                return Math.max(dest.retrievePageNumber(), 0);
+            }
+        } catch (Exception e) {
+            log.error("GetOutlinePage Error message: " + e);
         }
+        return -1;
     }
 
 }
