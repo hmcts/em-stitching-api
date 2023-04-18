876bd6454eb8e3827f2cee40d956013c9cb66ec6
Jefex7 <duartepireslopes@gmail.com>
2023-04-18 09:10:54 +0100

    Removed CheckStyle Errors for WelcomeResourceTest

--- src/test/java/uk/gov/hmcts/reform/em/stitching/rest/WelcomeResourceTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/stitching/rest/WelcomeResourceTest.java
@@ -3,36 +3,32 @@ package uk.gov.hmcts.reform.em.stitching.rest;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
 
 import java.util.Map;
-import java.util.Objects;
-
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-import static org.assertj.core.api.Assertions.assertThat;
+
 public class WelcomeResourceTest {
 
     private final WelcomeResource welcomeResource = new WelcomeResource();
 
     @Test
     public void testEndPointResponseCode() {
-
         ResponseEntity<Object> responseEntity = welcomeResource.welcome();
 
         assertNotNull(responseEntity);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
     }
+
     @Test
     public void testEndpointResponseMessage() throws JsonProcessingException {
-
         ResponseEntity<Object> responseEntity = welcomeResource.welcome();
 
         ObjectMapper mapper = new ObjectMapper();
-        String expectedResponse = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(Map.of("message" , "Welcome to Stitching API!"));
+        String expectedResponse = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(Map.of("message","Welcome to Stitching API!"));
 
         String actualResponse = (String) responseEntity.getBody();
         String cacheHeader = responseEntity.getHeaders().getCacheControl();
