375b8175e479ae3d7e471bc3e651c7016935a7be
Jefex7 <duartepireslopes@gmail.com>
2023-04-18 08:56:44 +0100

    Added Test to WelcomeResourceTest

--- src/test/java/uk/gov/hmcts/reform/em/stitching/rest/WelcomeResourceTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/stitching/rest/WelcomeResourceTest.java
@@ -1,24 +1,45 @@
 package uk.gov.hmcts.reform.em.stitching.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
-import org.skyscreamer.jsonassert.JSONAssert;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.HttpStatus;
+
+import java.util.Map;
+import java.util.Objects;
+
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
-
+import static org.assertj.core.api.Assertions.assertThat;
 public class WelcomeResourceTest {
 
     private final WelcomeResource welcomeResource = new WelcomeResource();
 
     @Test
-    public void test_should_return_welcome_response() {
+    public void testEndPointResponseCode() {
 
         ResponseEntity<Object> responseEntity = welcomeResource.welcome();
-        String expectedJson = "{\"message\":\"Welcome to Stitching API!\"}";
 
         assertNotNull(responseEntity);
         assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
-        JSONAssert.assertEquals(expectedJson, String.valueOf(responseEntity.getBody()),true);
+    }
+    @Test
+    public void testEndpointResponseMessage() throws JsonProcessingException {
+
+        ResponseEntity<Object> responseEntity = welcomeResource.welcome();
+
+        ObjectMapper mapper = new ObjectMapper();
+        String expectedResponse = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(Map.of("message" , "Welcome to Stitching API!"));
+
+        String actualResponse = (String) responseEntity.getBody();
+        String cacheHeader = responseEntity.getHeaders().getCacheControl();
+
+        assertNotNull(responseEntity);
+        assertEquals("no-cache",cacheHeader);
+        assertEquals(expectedResponse, actualResponse);
     }
 }
+
