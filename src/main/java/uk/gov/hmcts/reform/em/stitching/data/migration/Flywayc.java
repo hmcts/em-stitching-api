447040f647daba82c3262d62c7b7c7d5a860b819
mdayican <mustafa.dayican@hmcts.net>
2023-05-19 15:24:13 +0100

    custom FlywayConfigurationCustomizer

--- src/main/java/uk/gov/hmcts/reform/em/stitching/data/migration/Flywayc.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/data/migration/Flywayc.java
@@ -1,8 +1,6 @@
 package uk.gov.hmcts.reform.em.stitching.data.migration;
 
 import org.flywaydb.core.api.configuration.FluentConfiguration;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +8,8 @@ import java.util.Map;
 
 @Component
 public class Flywayc implements FlywayConfigurationCustomizer {
-    private final Logger log = LoggerFactory.getLogger(Flywayc.class);
-
     @Override
     public void customize(FluentConfiguration configuration) {
-        log.info("setting flyway configs ");
         configuration.configuration(Map.of("flyway.postgresql.transactional.lock","false"));
         configuration.envVars();
     }
