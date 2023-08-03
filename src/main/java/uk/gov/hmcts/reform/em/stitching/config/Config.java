5b5053121e6b55e5939de462bbc998b8038f03d7
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2023-08-03 19:06:18 +0100

    Removed unused import

--- src/main/java/uk/gov/hmcts/reform/em/stitching/config/Config.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/config/Config.java
@@ -1,6 +1,5 @@
 package uk.gov.hmcts.reform.em.stitching.config;
 
-import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
