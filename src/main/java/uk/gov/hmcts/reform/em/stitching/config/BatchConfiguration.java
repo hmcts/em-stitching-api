fdeb52567a1ebb5eada0ce5ce2d06eb1e5cf65fe
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2023-08-03 18:57:25 +0100

    Updated SpringBoot 3 batch config

--- src/main/java/uk/gov/hmcts/reform/em/stitching/config/BatchConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/config/BatchConfiguration.java
@@ -1,10 +1,9 @@
 package uk.gov.hmcts.reform.em.stitching.config;
 
-import jakarta.persistence.EntityManagerFactory;
 import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.JobParametersInvalidException;
@@ -20,15 +19,16 @@ import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.item.database.JpaItemWriter;
 import org.springframework.batch.item.database.JpaPagingItemReader;
 import org.springframework.batch.item.database.builder.JpaPagingItemReaderBuilder;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.transaction.PlatformTransactionManager;
 import uk.gov.hmcts.reform.em.stitching.batch.DocumentTaskCallbackProcessor;
 import uk.gov.hmcts.reform.em.stitching.batch.DocumentTaskItemProcessor;
 import uk.gov.hmcts.reform.em.stitching.batch.RemoveOldDocumentTaskTasklet;
@@ -37,6 +37,7 @@ import uk.gov.hmcts.reform.em.stitching.domain.DocumentTask;
 import uk.gov.hmcts.reform.em.stitching.info.BuildInfo;
 import uk.gov.hmcts.reform.em.stitching.repository.DocumentTaskRepository;
 
+import jakarta.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.random.RandomGenerator;
 
@@ -46,6 +47,9 @@ import java.util.random.RandomGenerator;
 @Configuration
 @ConditionalOnProperty(name = "scheduling.enabled")
 public class BatchConfiguration {
+
+    @Autowired
+    ResourcelessTransactionManager transactionManager;
 
     @Autowired
     JobRepository jobRepository;
@@ -188,7 +192,7 @@ public class BatchConfiguration {
     @Bean
     public Step step1() {
         return new StepBuilder("step1", this.jobRepository)
-                .<DocumentTask, DocumentTask>chunk(10, jobATransactionManager())
+                .<DocumentTask, DocumentTask>chunk(10, transactionManager)
                 .reader(newDocumentTaskReader())
                 .processor(documentTaskItemProcessor)
                 .writer(itemWriter())
@@ -207,7 +211,7 @@ public class BatchConfiguration {
     @Bean
     public Step callBackStep1() {
         return new StepBuilder("callbackStep1", this.jobRepository)
-                .<DocumentTask, DocumentTask>chunk(10, jobATransactionManager())
+                .<DocumentTask, DocumentTask>chunk(10, transactionManager)
                 .reader(completedWithCallbackDocumentTaskReader())
                 .processor(documentTaskCallbackProcessor)
                 .writer(itemWriter())
@@ -221,7 +225,7 @@ public class BatchConfiguration {
                 .flow(new StepBuilder("deleteAllExpiredBatchExecutions", this.jobRepository)
                         .tasklet(
                                 new RemoveSpringBatchHistoryTasklet(historicExecutionsRetentionMilliseconds, jdbcTemplate),
-                                jobATransactionManager()
+                                transactionManager
                         )
                         .build()).build().build();
     }
@@ -232,14 +236,8 @@ public class BatchConfiguration {
                 .flow(new StepBuilder("deleteAllHistoricalDocumentTaskRecords", this.jobRepository)
                         .tasklet(
                                 new RemoveOldDocumentTaskTasklet(documentTaskRepository, numberOfDays, numberOfRecords),
-                                jobATransactionManager())
+                                transactionManager)
                         .build()).build().build();
     }
 
-    @Bean
-    JpaTransactionManager jobATransactionManager() {
-        JpaTransactionManager transactionManager = new JpaTransactionManager();
-        transactionManager.setEntityManagerFactory(entityManagerFactory);
-        return transactionManager;
-    }
 }
--- src/main/java/uk/gov/hmcts/reform/em/stitching/config/Config.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/config/Config.java
@@ -1,10 +1,17 @@
 package uk.gov.hmcts.reform.em.stitching.config;
 
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.reform.idam.client.IdamApi;
 
 @Configuration
 @EnableFeignClients(basePackageClasses = {IdamApi.class})
 public class Config {
+
+    @Bean
+    public ResourcelessTransactionManager transactionManager() {
+        return new ResourcelessTransactionManager();
+    }
 }
