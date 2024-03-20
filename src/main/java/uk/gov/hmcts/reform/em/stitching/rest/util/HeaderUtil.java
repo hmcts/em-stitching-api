86e5cee5ec10d3ecc24a2c50bb96671ee145c75f
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-03-20 14:08:49 +0000

    fix more code smells

--- src/main/java/uk/gov/hmcts/reform/em/stitching/rest/util/HeaderUtil.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/rest/util/HeaderUtil.java
@@ -27,7 +27,7 @@ public final class HeaderUtil {
         return createAlert("A new " + entityName + " is created with identifier " + param, param);
     }
 
-    public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
+    public static HttpHeaders createFailureAlert(String entityName, String defaultMessage) {
         log.error("Entity processing failed, {}", defaultMessage);
         HttpHeaders headers = new HttpHeaders();
         headers.add("X-" + APPLICATION_NAME + "-error", defaultMessage);
