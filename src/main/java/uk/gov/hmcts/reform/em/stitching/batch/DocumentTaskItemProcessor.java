3596140b870f8feea58ef2fd63f98beebfcacdc4
michaeljacob <Michael.Jacob@HMCTS.NET>
2024-04-08 14:11:15 +0100

    checkstyle

--- src/main/java/uk/gov/hmcts/reform/em/stitching/batch/DocumentTaskItemProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/batch/DocumentTaskItemProcessor.java
@@ -154,7 +154,7 @@ public class DocumentTaskItemProcessor implements ItemProcessor<DocumentTask, Do
             if (Objects.nonNull(outputFile)) {
                 Path path = outputFile.toPath();
                 if (Objects.nonNull(path)) {
-                    if (Files.deleteIfExists(outputFile.toPath())){
+                    if (Files.deleteIfExists(outputFile.toPath())) {
                         log.info("File successfully deleted: {}", outputFile);
                     }
                 }
