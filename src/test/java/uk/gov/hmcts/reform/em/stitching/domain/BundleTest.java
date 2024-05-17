b8087f6d0e1a4068bfd4ef6a9b6d9e41015ad21d
mdayican <mdayican@gmail.com>
2024-05-17 10:43:33 +0100

    fix checkstyle

--- src/test/java/uk/gov/hmcts/reform/em/stitching/domain/BundleTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/stitching/domain/BundleTest.java
@@ -15,7 +15,6 @@ import java.time.Instant;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
