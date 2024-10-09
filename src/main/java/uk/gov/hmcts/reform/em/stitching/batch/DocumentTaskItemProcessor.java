7a709db4e811517a1289499da6826f317ff0a9ff
mdayican <mustafa.dayican1@hmcts.net>
2024-10-09 10:02:26 +0100

    EM-6266 update state before process #2396 (#2413)
    * EM-6266 update state before process
    
    * use REQUIRED
    
    * show sql
    
    * info level logs
    
    * revert logging
    
    * fix tansactional
    
    * add new class to create new transaction
    
    * change to debug level;
    
    * fix logs
    
    ---------
    
    Co-authored-by: yogesh-hullatti <53007108+yogesh-hullatti@users.noreply.github.com>

--- src/main/java/uk/gov/hmcts/reform/em/stitching/batch/DocumentTaskItemProcessor.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/batch/DocumentTaskItemProcessor.java
@@ -31,12 +31,17 @@ import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 import static pl.touk.throwing.ThrowingFunction.unchecked;
+import static uk.gov.hmcts.reform.em.stitching.config.BatchConfiguration.DOCUMENT_TASK_RETRY_COUNT;
 
 @Service
 @Transactional(propagation = Propagation.REQUIRED)
 @SuppressWarnings("java:S899")
 public class DocumentTaskItemProcessor implements ItemProcessor<DocumentTask, DocumentTask> {
     private final Logger log = LoggerFactory.getLogger(DocumentTaskItemProcessor.class);
+
+    private static final String FAILURE_DESCRIPTION_TEMPLATE =
+            "Document taskId %d, caseId: %s reached max retry count: %d";
+
     private final DmStoreDownloader dmStoreDownloader;
     private final DmStoreUploader dmStoreUploader;
     private final DocumentConversionService documentConverter;
@@ -45,13 +50,18 @@ public class DocumentTaskItemProcessor implements ItemProcessor<DocumentTask, Do
     private final PDFWatermark pdfWatermark;
     private final CdamService cdamService;
 
-    public DocumentTaskItemProcessor(DmStoreDownloader dmStoreDownloader,
-                                     DmStoreUploader dmStoreUploader,
-                                     DocumentConversionService documentConverter,
-                                     PDFMerger pdfMerger,
-                                     DocmosisClient docmosisClient,
-                                     PDFWatermark pdfWatermark,
-                                     CdamService cdamService) {
+    private final StoreDocumentTaskRetryCount storeDocumentTaskRetryCount;
+
+    public DocumentTaskItemProcessor(
+            DmStoreDownloader dmStoreDownloader,
+            DmStoreUploader dmStoreUploader,
+            DocumentConversionService documentConverter,
+            PDFMerger pdfMerger,
+            DocmosisClient docmosisClient,
+            PDFWatermark pdfWatermark,
+            CdamService cdamService,
+            StoreDocumentTaskRetryCount storeDocumentTaskRetryCount
+    ) {
         this.dmStoreDownloader = dmStoreDownloader;
         this.dmStoreUploader = dmStoreUploader;
         this.documentConverter = documentConverter;
@@ -59,6 +69,7 @@ public class DocumentTaskItemProcessor implements ItemProcessor<DocumentTask, Do
         this.docmosisClient = docmosisClient;
         this.pdfWatermark = pdfWatermark;
         this.cdamService = cdamService;
+        this.storeDocumentTaskRetryCount = storeDocumentTaskRetryCount;
     }
 
     @Override
@@ -69,7 +80,20 @@ public class DocumentTaskItemProcessor implements ItemProcessor<DocumentTask, Do
         stopwatch.start();
         Map<BundleDocument, File> bundleFiles = null;
         File outputFile = null;
-        documentTask.setRetryAttempts(documentTask.getRetryAttempts() + 1);
+
+        if (documentTask.getRetryAttempts() >= DOCUMENT_TASK_RETRY_COUNT - 1) {
+            documentTask.setTaskState(TaskState.FAILED);
+            String errorDescription = String.format(
+                    FAILURE_DESCRIPTION_TEMPLATE,
+                    documentTask.getId(),
+                    documentTask.getCaseTypeId(),
+                    DOCUMENT_TASK_RETRY_COUNT
+            );
+            documentTask.setFailureDescription(errorDescription);
+            log.error(errorDescription);
+            return documentTask;
+        }
+        this.storeDocumentTaskRetryCount.incrementRetryAttempts(documentTask);
         log.info(
             "DocumentTask : {}, CoverPage template {}",
             documentTask.getId(),
