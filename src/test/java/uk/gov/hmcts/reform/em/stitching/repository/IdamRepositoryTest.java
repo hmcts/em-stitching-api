6d4a6ddbe9f2a99be198d9c189fe3809f6a8191b
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2024-12-19 16:29:33 +0000

    Tests updated to use JUnit5 annotations

--- src/test/java/uk/gov/hmcts/reform/em/stitching/repository/IdamRepositoryTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/stitching/repository/IdamRepositoryTest.java
@@ -1,8 +1,7 @@
 package uk.gov.hmcts.reform.em.stitching.repository;
 
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
@@ -11,11 +10,13 @@ import uk.gov.hmcts.reform.idam.client.models.UserInfo;
 
 import static java.util.Arrays.asList;
 import static org.apache.commons.lang3.RandomStringUtils.random;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.util.AssertionErrors.assertNull;
 
-public class IdamRepositoryTest {
+class IdamRepositoryTest {
 
     @Mock
     private IdamClient idamClient;
@@ -25,14 +26,14 @@ public class IdamRepositoryTest {
     private static final  String FORE_NAME = "ABC";
     private static final  String SURNAME = "XYZ";
 
-    @Before
+    @BeforeEach
     public void setup() {
         MockitoAnnotations.openMocks(this);
         idamRepository = new IdamRepository(idamClient);
     }
 
     @Test
-    public void getUserDetailsTestSuccess() {
+    void getUserDetailsTestSuccess() {
 
         final UserInfo userInfo = UserInfo.builder()
             .uid("100")
@@ -43,18 +44,18 @@ public class IdamRepositoryTest {
         Mockito.when(idamClient.getUserInfo(Mockito.anyString())).thenReturn(userInfo);
         String token = random(5, true, false);
 
-        Assert.assertEquals(FORE_NAME,  idamRepository.getUserInfo(token).getGivenName());
-        Assert.assertEquals(SURNAME,  idamRepository.getUserInfo(token).getFamilyName());
+        assertEquals(FORE_NAME,  idamRepository.getUserInfo(token).getGivenName());
+        assertEquals(SURNAME,  idamRepository.getUserInfo(token).getFamilyName());
 
         verify(idamClient, times(2)).getUserInfo(anyString());
     }
 
     @Test
-    public void getUserDetailsTestFailure() {
+    void getUserDetailsTestFailure() {
 
         String token = "randomValue";
 
-        Assert.assertNull(FORE_NAME,  idamRepository.getUserInfo(token));
+        assertNull(FORE_NAME,  idamRepository.getUserInfo(token));
     }
 
 }
