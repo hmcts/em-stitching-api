9b17e2e002e01a808f71e12cbd34b3ff1dbb0adc
lars lunde <lars.lunde@hmcts.net>
2025-01-27 09:57:29 +0000

    opening PR to revert smoketest

--- src/aat/java/uk/gov/hmcts/reform/em/stitching/smoke/SmokeTest.java
+++ src/aat/java/uk/gov/hmcts/reform/em/stitching/smoke/SmokeTest.java
@@ -2,21 +2,21 @@ package uk.gov.hmcts.reform.em.stitching.smoke;
 
 import net.serenitybdd.annotations.WithTag;
 import net.serenitybdd.annotations.WithTags;
-import net.serenitybdd.junit5.SerenityJUnit5Extension;
+import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
 import net.serenitybdd.rest.SerenityRest;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import uk.gov.hmcts.reform.em.EmTestConfig;
 import uk.gov.hmcts.reform.em.stitching.testutil.TestUtil;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 @SpringBootTest(classes = {TestUtil.class, EmTestConfig.class})
 @TestPropertySource(value = "classpath:application.yml")
-@ExtendWith(SerenityJUnit5Extension.class)
+@RunWith(SpringIntegrationSerenityRunner.class)
 @WithTags({@WithTag("testType:Smoke")})
 public class SmokeTest {
 
