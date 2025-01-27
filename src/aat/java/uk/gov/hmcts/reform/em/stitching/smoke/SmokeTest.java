b99bae1a573d95c0a496d9d8ea59818c5cab5f90
lars lunde <lars.lunde@hmcts.net>
2025-01-27 10:59:08 +0000

    config basetest to use junit platform

--- src/aat/java/uk/gov/hmcts/reform/em/stitching/smoke/SmokeTest.java
+++ src/aat/java/uk/gov/hmcts/reform/em/stitching/smoke/SmokeTest.java
@@ -2,10 +2,10 @@ package uk.gov.hmcts.reform.em.stitching.smoke;
 
 import net.serenitybdd.annotations.WithTag;
 import net.serenitybdd.annotations.WithTags;
-import net.serenitybdd.junit.spring.integration.SpringIntegrationSerenityRunner;
+import net.serenitybdd.junit5.SerenityJUnit5Extension;
 import net.serenitybdd.rest.SerenityRest;
 import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
@@ -16,7 +16,7 @@ import static org.junit.Assert.assertEquals;
 
 @SpringBootTest(classes = {TestUtil.class, EmTestConfig.class})
 @TestPropertySource(value = "classpath:application.yml")
-@RunWith(SpringIntegrationSerenityRunner.class)
+@ExtendWith(SerenityJUnit5Extension.class)
 @WithTags({@WithTag("testType:Smoke")})
 public class SmokeTest {
 
