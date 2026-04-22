29d3859ab1cdbdcb595b2aae4cf9474accedbc91
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2026-04-22 13:49:16 +0100

    Updated callback url validator logic

--- src/main/java/uk/gov/hmcts/reform/em/stitching/domain/validation/CallableEndpointValidator.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/domain/validation/CallableEndpointValidator.java
@@ -2,52 +2,47 @@ package uk.gov.hmcts.reform.em.stitching.domain.validation;
 
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.Response;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.net.URI;
 import java.net.URL;
-
-import static uk.gov.hmcts.reform.em.stitching.service.CloseableCloser.close;
 
 public class CallableEndpointValidator implements ConstraintValidator<CallableEndpoint, String> {
 
     private final Logger log = LoggerFactory.getLogger(CallableEndpointValidator.class);
 
-    private final OkHttpClient okHttpClient;
+    private static final String PROD = "PROD";
 
-    public CallableEndpointValidator(OkHttpClient okHttpClient) {
-        this.okHttpClient = okHttpClient;
+    private final String allowedHost;
+    private final String env;
+
+    public CallableEndpointValidator(@Value("${callbackurlvalidator.host}") String host,
+                                     @Value("${callbackurlvalidator.env}") String env) {
+        this.allowedHost = host;
+        this.env = env;
     }
 
     @Override
     public boolean isValid(String urlString, ConstraintValidatorContext context) {
-        boolean valid;
-        Response response = null;
-        try {
-            URL url = new URI(urlString).toURL();
-            String urlWithoutPathString = String.format("%s://%s:%d",
-                    url.getProtocol(),
-                    url.getHost(),
-                    url.getPort() < 0 ? url.getDefaultPort() : url.getPort());
-            log.debug("Probing callback {}", urlWithoutPathString);
-            URL urlWithoutPath = new URI(urlWithoutPathString).toURL();
-            response = okHttpClient
-                    .newCall(new Request.Builder()
-                            .url(urlWithoutPath)
-                            .build())
-                    .execute();
-            valid = response.code() < 500;
-        } catch (Exception e) {
-            log.error(String.format("Callback %s could not be verified", urlString), e);
-            valid = false;
-        } finally {
-            close(response);
-        }
-        return valid;
-    }
 
+        if (StringUtils.isBlank(urlString)) {
+            return false;
+        }
+
+        if (env.equalsIgnoreCase(PROD)) {
+            URL url = null;
+            try {
+                url = new URI(urlString).toURL();
+            } catch (Exception e) {
+                log.error(String.format("Callback %s could not be verified", urlString), e);
+                return false;
+            }
+            return url.getHost().equalsIgnoreCase(allowedHost);
+        } else {
+            return urlString.contains(allowedHost);
+        }
+    }
 }
