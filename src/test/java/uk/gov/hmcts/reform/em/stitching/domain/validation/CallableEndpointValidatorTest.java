b9b722c13d01571ded2267b8455b5952f0ce9ba7
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2026-04-30 20:17:32 +0100

    Updated unit tests

--- src/test/java/uk/gov/hmcts/reform/em/stitching/domain/validation/CallableEndpointValidatorTest.java
+++ src/test/java/uk/gov/hmcts/reform/em/stitching/domain/validation/CallableEndpointValidatorTest.java
@@ -1,6 +1,7 @@
 package uk.gov.hmcts.reform.em.stitching.domain.validation;
 
 import jakarta.validation.ConstraintValidatorContext;
+import org.jspecify.annotations.NonNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -11,6 +12,10 @@ class CallableEndpointValidatorTest {
 
     private CallableEndpointValidator validator;
     private ConstraintValidatorContext mockContext;
+
+    private static final String PROD_URL = "PRODURL";
+    private static final String PROD = "PROD";
+    private static final String VALID_PROD_URL = "http://PRODURL:8080/api/stitching-complete-callback/1234567890123456/asyncStitchingComplete/123e4567-e89b-12d3-a456-426614174000";
 
     @BeforeEach
     void setUp() {
@@ -39,22 +44,37 @@ class CallableEndpointValidatorTest {
 
     @Test
     void isValidReturnsTrueForProdPerfectMatch() {
-        validator = new CallableEndpointValidator("PRODURL", "PROD");
-        String validUrl = "http://PRODURL:8080/api/stitching-complete-callback/1234567890123456/asyncStitchingComplete/123e4567-e89b-12d3-a456-426614174000";
-        assertTrue(validator.isValid(validUrl, mockContext));
+        validator = getValidator(PROD_URL, PROD);
+        assertTrue(validator.isValid(VALID_PROD_URL, mockContext));
     }
 
     @Test
     void isValidReturnsTrueForProdNonPerfectMatch() {
-        validator = new CallableEndpointValidator("PRODURL", "PROD");
+        validator = getValidator(PROD_URL, PROD);
         String validUrl = "http://PRODURLNONMATCH:8080/api/stitching-complete-callback/1234567890123456/asyncStitchingComplete/123e4567-e89b-12d3-a456-426614174000";
         assertFalse(validator.isValid(validUrl, mockContext));
     }
 
     @Test
     void isValidReturnsFalseWhenUrlIsMalformed() {
-        validator = new CallableEndpointValidator("some-host", "PROD");
+        validator = getValidator("some-host", PROD);
         String malformedUrl = "http://[invalid-url"; // invalid URI that will throw
         assertFalse(validator.isValid(malformedUrl, mockContext));
+    }
+
+    @Test
+    void isValidReturnsFalseWhenHostIsBlank() {
+        validator = getValidator(" ", PROD);
+        assertFalse(validator.isValid(VALID_PROD_URL, mockContext));
+    }
+
+    @Test
+    void isValidReturnsFalseWhenOneOfHostIsEmpty() {
+        validator = getValidator("dummy, ", PROD);
+        assertFalse(validator.isValid(VALID_PROD_URL, mockContext));
+    }
+
+    private static @NonNull CallableEndpointValidator getValidator(String hosts, String env) {
+        return new CallableEndpointValidator(hosts, env);
     }
 }
