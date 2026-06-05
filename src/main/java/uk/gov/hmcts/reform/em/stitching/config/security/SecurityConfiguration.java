65c6336e931c9428c04cd7357a0c6644e9ed9a93
Michael Jacob <88201349+MichaelJacob21012@users.noreply.github.com>
2026-06-05 11:47:32 +0100

    add logging for issuer validation (#3315)

--- src/main/java/uk/gov/hmcts/reform/em/stitching/config/security/SecurityConfiguration.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/config/security/SecurityConfiguration.java
@@ -1,5 +1,7 @@
 package uk.gov.hmcts.reform.em.stitching.config.security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,7 +25,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import uk.gov.hmcts.reform.authorisation.filters.ServiceAuthFilter;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
 @Configuration
@@ -33,6 +34,8 @@ public class SecurityConfiguration {
 
     @Value("${spring.security.oauth2.client.provider.oidc.issuer-uri}")
     private String issuerUri;
+
+    private static final Logger log = LoggerFactory.getLogger(SecurityConfiguration.class);
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
@@ -78,6 +81,15 @@ public class SecurityConfiguration {
 
     OAuth2TokenValidator<Jwt> allowedIssuersValidator(List<String> allowedIssuers) {
         Set<String> allowedIssuerSet = Set.copyOf(allowedIssuers);
-        return new JwtClaimValidator<>("iss", iss -> Objects.nonNull(iss) && allowedIssuerSet.contains(iss));
+
+        return new JwtClaimValidator<>("iss", iss -> isAllowedIssuer(iss, allowedIssuerSet));
+    }
+
+    private boolean isAllowedIssuer(Object iss, Set<String> allowedIssuerSet) {
+        if (iss instanceof String issuer && allowedIssuerSet.contains(issuer)) {
+            return true;
+        }
+        log.warn("JWT rejected: issuer not allowed. iss={}", iss);
+        return false;
     }
 }
