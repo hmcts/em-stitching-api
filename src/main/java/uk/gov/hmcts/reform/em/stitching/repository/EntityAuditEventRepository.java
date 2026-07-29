e4c079de218bf3445afe488d49546cacb6ce7b6b
michaeljacob <Michael.Jacob@HMCTS.NET>
2026-07-29 13:56:38 +0100

    use text block

--- src/main/java/uk/gov/hmcts/reform/em/stitching/repository/EntityAuditEventRepository.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/repository/EntityAuditEventRepository.java
@@ -31,12 +31,12 @@ public interface EntityAuditEventRepository extends JpaRepository<EntityAuditEve
     EntityAuditEvent findOneByEntityTypeAndEntityIdAndCommitVersion(@Param("type") String type, @Param("entityId")
             Long entityId, @Param("commitVersion") Integer commitVersion);
 
-    @Query(value =
-        "SELECT m.id FROM jhi_entity_audit_event m "
-            + "WHERE m.modified_date <= :modifiedDate "
-            + "ORDER BY m.modified_date ASC "
-            + "LIMIT :numberOfRecords",
-        nativeQuery = true)
+    @Query(value = """
+            SELECT m.id FROM jhi_entity_audit_event m
+            WHERE m.modified_date <= :modifiedDate
+            ORDER BY m.modified_date ASC
+            LIMIT :numberOfRecords
+            """, nativeQuery = true)
     List<Long> findAllByModifiedDate(
         @Param("modifiedDate") Instant modifiedDate,
         @Param("numberOfRecords") int numberOfRecords
