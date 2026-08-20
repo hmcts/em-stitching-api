42d45dd25a8c9ecf78926032602175c0b6a71fdf
Michael Jacob <88201349+MichaelJacob21012@users.noreply.github.com>
2026-08-20 17:14:12 +0100

    add subtitle config columns (#3423)

---
+++ src/main/resources/db/migration/V17__add_subtitle_config_columns.sql
@@ -0,0 +1,2 @@
+ALTER TABLE public.bundle ADD COLUMN has_table_of_contents_subtitles boolean DEFAULT false;
+ALTER TABLE public.bundle ADD COLUMN has_document_outline_subtitles boolean DEFAULT false;
