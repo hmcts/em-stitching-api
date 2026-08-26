89c1de302362b88ff3596f120aef6f6a960fc075
michaeljacob <Michael.Jacob@HMCTS.NET>
2026-08-26 17:50:23 +0100

    Revert "update exception"
    This reverts commit 9e3e1bd6806af9970aa1dfff457e09fdf51d516b.

--- src/main/java/uk/gov/hmcts/reform/em/stitching/service/impl/DocumentTaskProcessingException.java
+++ src/main/java/uk/gov/hmcts/reform/em/stitching/service/impl/DocumentTaskProcessingException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class DocumentTaskProcessingException extends RuntimeException {
+public class DocumentTaskProcessingException extends Exception {
 
     public DocumentTaskProcessingException(String message) {
         super(message);
